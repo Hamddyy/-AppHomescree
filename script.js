6bc23c5e0be99533eb142c5b8d9f07b16fdedb17
Hamddyy <hamddyy.mansour@gmail.com>
2024-12-27 06:41:45 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,312 @@
+$("#searchTemplatesCarousel").slick({
+  centerMode: false,
+  centerPadding: "0px",
+  speed: 0,
+  slidesToShow: 6,
+  slidesToScroll: 1,
+  autoplay: false,
+  autoplaySpeed: 3500,
+  arrows: true,
+  dots: false,
+  infinite: true,
+  pauseOnHover: false,
+  pauseOnFocus: false,
+  touchMove: true,
+  swipe: true,
+  draggable: true,
+  swipeToSlide: true,
+  responsive: [
+    {
+      breakpoint: 1080,
+      settings: {
+        slidesToShow: 4
+      }
+    }
+  ]
+});
+$("#searchQuickCarousel").slick({
+  centerMode: false,
+  centerPadding: "0px",
+  speed: 0,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: false,
+  autoplaySpeed: 3500,
+  arrows: true,
+  dots: false,
+  infinite: true,
+  pauseOnHover: false,
+  pauseOnFocus: false,
+  touchMove: true,
+  swipe: true,
+  draggable: true,
+  swipeToSlide: true,
+  responsive: [
+    {
+      breakpoint: 1080,
+      settings: {
+        slidesToShow: 2
+      }
+    }
+  ]
+});
+$("#suggestedCarousel").slick({
+  centerMode: false,
+  centerPadding: "0px",
+  speed: 0,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: false,
+  autoplaySpeed: 3500,
+  arrows: true,
+  dots: false,
+  infinite: true,
+  pauseOnHover: false,
+  pauseOnFocus: false,
+  touchMove: true,
+  swipe: true,
+  draggable: true,
+  swipeToSlide: true,
+  responsive: [
+    {
+      breakpoint: 1170,
+      settings: {
+        slidesToShow: 2
+      }
+    },
+    {
+      breakpoint: 920,
+      settings: {
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+$("#expressCarousel").slick({
+  centerMode: false,
+  centerPadding: "0px",
+  speed: 0,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: false,
+  autoplaySpeed: 3500,
+  arrows: true,
+  dots: false,
+  infinite: true,
+  pauseOnHover: false,
+  pauseOnFocus: false,
+  touchMove: true,
+  swipe: true,
+  draggable: true,
+  swipeToSlide: true,
+  responsive: [
+    {
+      breakpoint: 1170,
+      settings: {
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 920,
+      settings: {
+        slidesToShow: 2
+      }
+    },
+    {
+      breakpoint: 790,
+      settings: {
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+$("#fireflyCarousel").slick({
+  centerMode: false,
+  centerPadding: "0px",
+  speed: 0,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: false,
+  autoplaySpeed: 3500,
+  arrows: true,
+  dots: false,
+  infinite: true,
+  pauseOnHover: false,
+  pauseOnFocus: false,
+  touchMove: true,
+  swipe: true,
+  draggable: true,
+  swipeToSlide: true,
+  responsive: [
+    {
+      breakpoint: 1170,
+      settings: {
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 920,
+      settings: {
+        slidesToShow: 2
+      }
+    },
+    {
+      breakpoint: 790,
+      settings: {
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+$("#popularCarousel").slick({
+  centerMode: false,
+  centerPadding: "0px",
+  speed: 0,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: false,
+  autoplaySpeed: 3500,
+  arrows: true,
+  dots: false,
+  infinite: true,
+  pauseOnHover: false,
+  pauseOnFocus: false,
+  touchMove: true,
+  swipe: true,
+  draggable: true,
+  swipeToSlide: true,
+  responsive: [
+    {
+      breakpoint: 1170,
+      settings: {
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 920,
+      settings: {
+        slidesToShow: 2
+      }
+    },
+    {
+      breakpoint: 790,
+      settings: {
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+$("#photoCarousel").slick({
+  centerMode: false,
+  centerPadding: "0px",
+  speed: 0,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: false,
+  autoplaySpeed: 3500,
+  arrows: true,
+  dots: false,
+  infinite: true,
+  pauseOnHover: false,
+  pauseOnFocus: false,
+  touchMove: true,
+  swipe: true,
+  draggable: true,
+  swipeToSlide: true,
+  responsive: [
+    {
+      breakpoint: 1170,
+      settings: {
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 920,
+      settings: {
+        slidesToShow: 2
+      }
+    },
+    {
+      breakpoint: 790,
+      settings: {
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+$("#videoCarousel").slick({
+  centerMode: false,
+  centerPadding: "0px",
+  speed: 0,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: false,
+  autoplaySpeed: 3500,
+  arrows: true,
+  dots: false,
+  infinite: true,
+  pauseOnHover: false,
+  pauseOnFocus: false,
+  touchMove: true,
+  swipe: true,
+  draggable: true,
+  swipeToSlide: true,
+  responsive: [
+    {
+      breakpoint: 1170,
+      settings: {
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 920,
+      settings: {
+        slidesToShow: 2
+      }
+    },
+    {
+      breakpoint: 790,
+      settings: {
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+$("#pdfCarousel").slick({
+  centerMode: false,
+  centerPadding: "0px",
+  speed: 0,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: false,
+  autoplaySpeed: 3500,
+  arrows: true,
+  dots: false,
+  infinite: true,
+  pauseOnHover: false,
+  pauseOnFocus: false,
+  touchMove: true,
+  swipe: true,
+  draggable: true,
+  swipeToSlide: true,
+  responsive: [
+    {
+      breakpoint: 1170,
+      settings: {
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 920,
+      settings: {
+        slidesToShow: 2
+      }
+    },
+    {
+      breakpoint: 790,
+      settings: {
+        slidesToShow: 1
+      }
+    }
+  ]
+});
